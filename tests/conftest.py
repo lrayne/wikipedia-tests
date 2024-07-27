fcc10790d6d04aedcb5ad4e70abeb3102dcbcf4d
lrayne <legaterayne@gmail.com>
2024-07-27 11:41:19 +0300

    Fix after review

--- tests/conftest.py
+++ tests/conftest.py
@@ -60,8 +60,6 @@ def local():
 
     browser.config.driver_name = config.driver_name
     browser.config.hold_driver_at_exit = config.hold_driver_at_exit
-    browser.config.window_width = config.window_width
-    browser.config.window_height = config.window_height
     browser.config.timeout = config.timeout
     browser.config._wait_decorator = support._logging.wait_with(
         context=allure_commons._allure.StepContext
